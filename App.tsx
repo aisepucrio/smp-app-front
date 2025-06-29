a12f82164286c5233d0b6f1b97f90679191cb165
tomytp <thomazrebellomiranda@gmail.com>
2025-06-29 11:31:53 -0300

    welcome screen

--- App.tsx
+++ App.tsx
@@ -1,18 +1,20 @@
-import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
 import { ActivityIndicator, View } from 'react-native';
 
 import { AuthProvider, useAuth } from './src/contexts/AuthContext';
+import MainTabs from './src/navigation/MainTabs';
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
-import MainTabs from './src/navigation/MainTabs';
+import WelcomeScreen from './src/screens/WelcomeScreen';
 
 const Stack = createNativeStackNavigator();
 
 function AuthStack() {
     return (
         <Stack.Navigator screenOptions={{ headerShown: false, animation: 'none' }}>
+            <Stack.Screen name="Welcome" component={WelcomeScreen} />
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="Register" component={RegisterScreen} />
         </Stack.Navigator>
